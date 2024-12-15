e76f6297d5aba00a028f112d89a80480798548f4
Jose Masri <ae_jmsalame@contractor.indeed.com>
2024-12-14 19:54:20 -0600

    Removed unnecesary token

--- portfolio-client/src/gql/client.ts
+++ portfolio-client/src/gql/client.ts
@@ -5,18 +5,12 @@ export default function () {
   if (typeof window !== "undefined") {
     return new ApolloClient({
       uri: `${process.env.NEXT_PUBLIC_API_BASE_URL}/graphql`,
-      cache: new InMemoryCache(),
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_API_TOKEN}`
-      }
+      cache: new InMemoryCache()
     });
   }
   // Server side
   return new ApolloClient({
     uri: `${process.env.API_BASE_URL}/graphql`,
-    cache: new InMemoryCache(),
-    headers: {
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_API_TOKEN}`
-    }
+    cache: new InMemoryCache()
   });
 }
